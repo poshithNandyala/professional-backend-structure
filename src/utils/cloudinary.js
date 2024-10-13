d909b09faf24588db5a3af1a14971e70ae6a295f
poshithNandyala <poshithiiitl@gmail.com>
2024-10-13 16:12:32 +0530

    completed project setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,48 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+import { ApiError } from "./ApiError.js"
+
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return new ApiError(
+            401,
+            "localfilePath not found"
+        )
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        })
+        //file successfully uploaded
+        console.log('file is uploaded on cloudinary', response.url);
+
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)  //remove the locally saved temporary file as the upload operation got failed
+        throw new ApiError(
+            500,
+            "cloudinary error"
+        )
+    }
+}
+
+export { uploadOnCloudinary }
+
+// Upload an image
+// const uploadResult = await cloudinary.uploader
+//     .upload(
+//         'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+//         public_id: 'shoes',
+//     }
+//     )
+//     .catch((error) => {
+//         console.log(error);
+//     });
