44ad415624dda49bf00b5778690be3600db66ad5
poshithNandyala <poshithiiitl@gmail.com>
2024-10-13 17:44:52 +0530

    First controller written

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import  dotenv  from 'dotenv';
-import { connectDB } from "./db/index.js";
 
+import dotenv from 'dotenv';
+import { connectDB } from "./db/index.js";
+import {app} from './app.js'
 dotenv.config(
     {
         path: '.env'
     }
 )
 
+
 connectDB()
+    .then(() => {
+        const port = process.env.PORT || 8000;
+        app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
+    })
+    .catch((err) => {
+        console.log('MongoDB connection failed', err);
+        process.exit(1);
+    });
